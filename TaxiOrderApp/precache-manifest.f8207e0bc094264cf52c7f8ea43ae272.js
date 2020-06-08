478c4d8b47a031ae2fb542db26d3fdbe7566be4c
My420 <my420V@gmail.com>
2020-06-08 19:04:21 +0300

    added TaxiOrderApp

---
+++ TaxiOrderApp/precache-manifest.f8207e0bc094264cf52c7f8ea43ae272.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d3c7272d71edbe2d1d2ed7a8be153fb",
+    "url": "/TaxiOrderApp/index.html"
+  },
+  {
+    "revision": "0d7f17cbb353901db7d5",
+    "url": "/TaxiOrderApp/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ed21971f17ae6988afb3",
+    "url": "/TaxiOrderApp/static/css/main.2e4b81b2.chunk.css"
+  },
+  {
+    "revision": "0d7f17cbb353901db7d5",
+    "url": "/TaxiOrderApp/static/js/2.aa58e3e5.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/TaxiOrderApp/static/js/2.aa58e3e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed21971f17ae6988afb3",
+    "url": "/TaxiOrderApp/static/js/main.aeb2a568.chunk.js"
+  },
+  {
+    "revision": "6351bbca213e11c44d8f",
+    "url": "/TaxiOrderApp/static/js/runtime-main.b58bde0e.js"
+  }
+]);
