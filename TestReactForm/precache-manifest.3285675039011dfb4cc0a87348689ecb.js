6a8d0a16eaade080695eb1d1f4cf9121e6c89c12
My420 <my420V@gmail.com>
2020-07-13 14:24:01 +0300

    add TestReactForm

---
+++ TestReactForm/precache-manifest.3285675039011dfb4cc0a87348689ecb.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28cafe1335fb53ecf73668b92b4d3683",
+    "url": "/TestReactForm/index.html"
+  },
+  {
+    "revision": "f3c62f54890e02d0d8bc",
+    "url": "/TestReactForm/static/css/main.95603247.chunk.css"
+  },
+  {
+    "revision": "7c3069c0cc1339a4308b",
+    "url": "/TestReactForm/static/js/2.3b592bd4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TestReactForm/static/js/2.3b592bd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3c62f54890e02d0d8bc",
+    "url": "/TestReactForm/static/js/main.6c17d5b2.chunk.js"
+  },
+  {
+    "revision": "079c531080f5c9fd34a1",
+    "url": "/TestReactForm/static/js/runtime-main.7a4d21ef.js"
+  },
+  {
+    "revision": "6747665ce25cde9619512b3e263b4788",
+    "url": "/TestReactForm/static/media/Roboto.6747665c.woff"
+  },
+  {
+    "revision": "a28925209cdc39a408c3d7880c7e5938",
+    "url": "/TestReactForm/static/media/Roboto.a2892520.woff2"
+  },
+  {
+    "revision": "5a7f489c7bcebe36b0e6fddc7319f6e7",
+    "url": "/TestReactForm/static/media/Robotomedium.5a7f489c.woff2"
+  },
+  {
+    "revision": "e95367ae977aeea9fbc8440f134d62a4",
+    "url": "/TestReactForm/static/media/Robotomedium.e95367ae.woff"
+  },
+  {
+    "revision": "5733893ec09af223c038a1747ce981aa",
+    "url": "/TestReactForm/static/media/SFCompactmedium.5733893e.woff"
+  },
+  {
+    "revision": "93b32b9842e4d31c9335366450042145",
+    "url": "/TestReactForm/static/media/SFCompactmedium.93b32b98.woff2"
+  },
+  {
+    "revision": "8bb2c15bca4d7901dfe4fa2a5c940046",
+    "url": "/TestReactForm/static/media/SFCompactregular.8bb2c15b.woff"
+  },
+  {
+    "revision": "a2a08cfb6b5e007bcaa749be47896979",
+    "url": "/TestReactForm/static/media/SFCompactregular.a2a08cfb.woff2"
+  },
+  {
+    "revision": "32c48621d0c410c0366e150d67de666d",
+    "url": "/TestReactForm/static/media/SFCompactsemibold.32c48621.woff2"
+  },
+  {
+    "revision": "e2eb3cc85b101d442cb237993edf6cbd",
+    "url": "/TestReactForm/static/media/SFCompactsemibold.e2eb3cc8.woff"
+  }
+]);
