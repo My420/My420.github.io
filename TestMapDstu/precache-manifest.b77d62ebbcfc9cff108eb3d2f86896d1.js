44a8606e73bf121fc489094ac2903f4a64a8560f
My420 <my420V@gmail.com>
2020-07-21 10:07:30 +0300

    added TestMapDstu

---
+++ TestMapDstu/precache-manifest.b77d62ebbcfc9cff108eb3d2f86896d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97a59be8e911cddb981a7ddde43a3527",
+    "url": "/TestMapDstu/index.html"
+  },
+  {
+    "revision": "f8a520bc9f54ca0482ee",
+    "url": "/TestMapDstu/static/css/2.68a70c26.chunk.css"
+  },
+  {
+    "revision": "67434a10869ef395ea71",
+    "url": "/TestMapDstu/static/css/main.1fbe211f.chunk.css"
+  },
+  {
+    "revision": "f8a520bc9f54ca0482ee",
+    "url": "/TestMapDstu/static/js/2.c08fc869.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/TestMapDstu/static/js/2.c08fc869.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67434a10869ef395ea71",
+    "url": "/TestMapDstu/static/js/main.2c9a5f0e.chunk.js"
+  },
+  {
+    "revision": "87be2a29498ed01fc1b7",
+    "url": "/TestMapDstu/static/js/runtime-main.2d07d706.js"
+  }
+]);
